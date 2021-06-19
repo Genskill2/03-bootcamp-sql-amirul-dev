cb9f41519b3fe5e2eade96c1d64e8bc249494347
Amirul Haqe <amiruldev1@gmail.com>
2021-06-19 12:56:21 +0530

    create.sql updated

--- create.sql
+++ create.sql
@@ -1,5 +1,5 @@
-create table publisher (id serial primary key, name text, country text);
-create table books (id serial primary key, title text, publisher serial references publisher(id));
-create table subjects (id serial primary key, name text);
-create table books_subjects (book serial references books(id), subject serial references subjects(id));
+create table publisher (id integer primary key, name text, country text);
+create table books (id integer primary key, title text, publisher integer references publisher(id));
+create table subjects (id integer primary key, name text);
+create table books_subjects (book integer references books(id), subject integer references subjects(id));
 

20e2b98ba89c0030c31803442d15d99102dc70f1
Amirul Haqe <amiruldev1@gmail.com>
2021-06-19 09:39:46 +0530

    solutions updated

--- query1.sql
+++ query1.sql
@@ -1,2 +1,2 @@
-select title from books where publisher = 'PHI';
+select title from books where publisher in (select id from publisher where name = 'PHI');
 
--- query3.sql
+++ query3.sql
@@ -1,1 +1,1 @@
-select b.books from books b, subjects s, books_subjects bs where bs.book = b.title and bs.subjects = s.name and s.name in ('Technology', 'Politics');
+select b.title from books b, subjects s, books_subjects bs where bs.book = b.id and bs.subject = s.id and s.name in ('Technology', 'Politics');

027c163b69c792153e168b43af4fb56ded33dc36
Amirul Haqe <amiruldev1@gmail.com>
2021-06-19 09:55:28 +0530

    create file updated

--- create.sql
+++ create.sql
@@ -1,5 +1,5 @@
 create table publisher (id serial primary key, name text, country text);
-create table books (id serial primary key, title text, publisher serial references publisher(id));
+create table books (id serial primary key, title text, publisher serial, foreign key(publisher) references publisher(id));
 create table subjects (id serial primary key, name text);
-create table books_subjects (book serial references books(id), subject serial references subjects(id));
+create table books_subjects (book serial, foreign key(book) references books(id), subject serial , foreign key(subject) references subjects(id));
 
